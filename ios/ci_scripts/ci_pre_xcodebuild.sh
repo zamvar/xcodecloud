d349caf8a32e9bacc9fabf490057e8c96c25c934
Ralph Pon-an <ralph@thinkbitsolutions.com>
2024-10-15 11:32:41 +0800

    feat: pre-xcodebuild

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,1 +1,28 @@
-# should flutterfire config be called
+#!/bin/bash
+
+# Path to the generated.xcconfig file
+CONFIG_FILE="ios/Flutter/generated.xcconfig"
+
+# Check if the SCHEME environment variable is set
+if [ -z "$SCHEME" ]; then
+    echo "Error: No scheme (flavor) provided!"
+    exit 1
+fi
+
+# Convert the scheme to lowercase
+FLAVOR=$(echo "$SCHEME" | tr '[:upper:]' '[:lower:]')
+
+# Construct the new FLUTTER_TARGET based on the flavor
+NEW_FLUTTER_TARGET="lib/main_${FLAVOR}.dart"
+
+# Check if the generated.xcconfig file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: File $CONFIG_FILE not found!"
+    exit 1
+fi
+
+# Use sed to update FLUTTER_TARGET
+sed -i '' "s|^FLUTTER_TARGET=.*|FLUTTER_TARGET=$NEW_FLUTTER_TARGET|" "$CONFIG_FILE"
+
+# Output result
+echo "FLUTTER_TARGET updated to $NEW_FLUTTER_TARGET in $CONFIG_FILE"
